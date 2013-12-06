012d7fec9f2c4ab5cb5dbba532e59dd7089807f3
Matt Wagantall <mattw@codeaurora.org>
2013-12-05 20:01:32 -0800

    sched/debug: Make sysrq prints of sched debug data optional
    Calls to sysrq_sched_debug_show() can yield rather verbose output
    which contributes to log spew and, under heavy load, may increase
    the chances of a watchdog bark.
    
    Make printing of this data optional with the introduction of a
    new Kconfig, CONFIG_SYSRQ_SCHED_DEBUG.
    
    Change-Id: I5f54d901d0dea403109f7ac33b8881d967a899ed
    Signed-off-by: Matt Wagantall <mattw@codeaurora.org>
    
    Conflicts:
    	kernel/sched/core.c

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5226,7 +5226,7 @@ void show_state_filter(unsigned long state_filter)
 			sched_show_task(p);
 	}
 
-#ifdef CONFIG_SCHED_DEBUG
+#ifdef CONFIG_SYSRQ_SCHED_DEBUG
 	sysrq_sched_debug_show();
 #endif
 	rcu_read_unlock();
