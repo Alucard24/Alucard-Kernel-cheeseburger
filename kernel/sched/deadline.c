3b2245310be34a4f6c5c2dd91c93308c0491050d
Olav Haugan <ohaugan@codeaurora.org>
2015-08-05 08:45:21 -0700

    sched: Update task->on_rq when tasks are moving between runqueues
    Task->on_rq has three states:
    	0 - Task is not on runqueue (rq)
    	1 (TASK_ON_RQ_QUEUED) - Task is on rq
    	2 (TASK_ON_RQ_MIGRATING) - Task is on rq but in the
    	process of being migrated to another rq
    
    When a task is moving between rqs task->on_rq state should be
    TASK_ON_RQ_MIGRATING
    
    CRs-fixed: 884720
    Change-Id: I1572aba00a0273d4ad5bc9a3dd60fb68e2f0b895
    Signed-off-by: Olav Haugan <ohaugan@codeaurora.org>

--- kernel/sched/deadline.c
+++ kernel/sched/deadline.c
@@ -291,9 +291,11 @@ static struct rq *dl_task_offline_migration(struct rq *rq, struct task_struct *p
 	/*
 	 * By now the task is replenished and enqueued; migrate it.
 	 */
+	p->on_rq = TASK_ON_RQ_MIGRATING;
 	deactivate_task(rq, p, 0);
 	set_task_cpu(p, later_rq->cpu);
 	activate_task(later_rq, p, 0);
+	p->on_rq = TASK_ON_RQ_QUEUED;
 
 	if (!fallback)
 		resched_curr(later_rq);
@@ -1712,6 +1714,7 @@ retry:
 		goto retry;
 	}
 
+	next_task->on_rq = TASK_ON_RQ_MIGRATING;
 	deactivate_task(rq, next_task, 0);
 	clear_average_bw(&next_task->dl, &rq->dl);
 	next_task->on_rq = TASK_ON_RQ_MIGRATING;
@@ -1719,6 +1722,7 @@ retry:
 	next_task->on_rq = TASK_ON_RQ_QUEUED;
 	add_average_bw(&next_task->dl, &later_rq->dl);
 	activate_task(later_rq, next_task, 0);
+	next_task->on_rq = TASK_ON_RQ_QUEUED;
 	ret = 1;
 
 	resched_curr(later_rq);
@@ -1804,6 +1808,7 @@ static void pull_dl_task(struct rq *this_rq)
 
 			resched = true;
 
+			p->on_rq = TASK_ON_RQ_MIGRATING;
 			deactivate_task(src_rq, p, 0);
 			clear_average_bw(&p->dl, &src_rq->dl);
 			p->on_rq = TASK_ON_RQ_MIGRATING;
@@ -1811,6 +1816,7 @@ static void pull_dl_task(struct rq *this_rq)
 			p->on_rq = TASK_ON_RQ_QUEUED;
 			add_average_bw(&p->dl, &this_rq->dl);
 			activate_task(this_rq, p, 0);
+			p->on_rq = TASK_ON_RQ_QUEUED;
 			dmin = p->dl.deadline;
 
 			/* Is there any other task even earlier? */
