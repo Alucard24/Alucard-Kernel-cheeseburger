336bb1dd1ea7e2a74f8919bd0b25c0c3719fb57f
Ulf Hansson <ulf.hansson@linaro.org>
2016-05-30 11:33:12 +0200

    PM / Runtime: Avoid resuming devices again in pm_runtime_force_resume()
    If the runtime PM status of the device isn't RPM_SUSPENDED, prevent the
    pm_runtime_force_resume() from invoking the ->runtime_resume() callback
    for the device, as it's not the expected behaviour from the subsystem/driver.
    
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Reviewed-by: Kevin Hilman <khilman@baylibre.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/base/power/runtime.c
+++ drivers/base/power/runtime.c
@@ -1469,6 +1469,9 @@ int pm_runtime_force_resume(struct device *dev)
 		goto out;
 	}
 
+	if (!pm_runtime_status_suspended(dev))
+		goto out;
+
 	ret = pm_runtime_set_active(dev);
 	if (ret)
 		goto out;
