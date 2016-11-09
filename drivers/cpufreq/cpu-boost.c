666305f7d95af36890a6c4821577fc741d8756cf
Francisco Franco <franciscofranco.1990@gmail.com>
2016-11-09 03:43:05 +0000

    cpufreq: cpu-boost: set interval between consecutive boosts to be at least the duration of the boost instead of a bigger value
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -49,7 +49,6 @@ int sched_dynamic_stune_boost = 0;
 
 static struct delayed_work input_boost_rem;
 static u64 last_input_time;
-#define MIN_INPUT_INTERVAL (150 * USEC_PER_MSEC)
 
 static int set_input_boost_freq(const char *buf, const struct kernel_param *kp)
 {
@@ -221,7 +220,7 @@ static void cpuboost_input_event(struct input_handle *handle,
 		return;
 
 	now = ktime_to_us(ktime_get());
-	if (now - last_input_time < MIN_INPUT_INTERVAL)
+	if ((now - last_input_time) < (input_boost_ms * USEC_PER_MSEC))
 		return;
 
 	if (work_pending(&input_boost_work))
