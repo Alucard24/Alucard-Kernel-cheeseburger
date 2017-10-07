716f50a3d65614f8769271a1338d957cfc266e0c
alucard24 <dmbaoh2@gmail.com>
2017-10-07 12:48:45 +0200

    Added building and flashing scripts

---
+++ READY-KERNEL/ramdisk/init.supolicy.sh
@@ -0,0 +1,20 @@
+#!/system/bin/sh
+
+SULIBS="/su/lib:/sbin/supersu/lib:/system/lib64:/system/lib"
+
+for SUPOLICY in `which supolicy sepolicy-inject`;
+do
+	LD_LIBRARY_PATH=$SULIBS $SUPOLICY --live \
+        "allow priv_app { cache_private_backup_file unlabeled } dir getattr" \
+        "allow untrusted_app sysfs_kgsl file { read write getattr open }" \
+        "allow untrusted_app sysfs_kgsl dir { read write getattr open }" \
+        "allow untrusted_app sysfs file { read write getattr open }" \
+        "allow untrusted_app sysfs dir { read write getattr open }" \
+        "allow { audioserver system_server location sensors } diag_device chr_file { read write }" \
+        "allow vold logd dir { read open }" \
+        "allow vold logd lnk_file { read getattr }" \
+        "allow dumpstate theme_data_file file getattr" \
+        "allow mediaserver mediaserver_tmpfs file { read write execute }" \
+        "allow shell dalvikcache_data_file dir write" \
+	"allow perfd system_server file write"
+done
