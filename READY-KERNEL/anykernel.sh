670f3e694ae7fa3f632c96af5f61001e7a33ba86
alucard24 <dmbaoh2@gmail.com>
2017-12-27 15:27:16 +0100

    Updated building script and ramdisk

--- READY-KERNEL/anykernel.sh
+++ READY-KERNEL/anykernel.sh
@@ -41,6 +41,9 @@ dump_boot;
 backup_file init.rc
 insert_line init.rc "init.qcom.power.rc" after "import init.qcom.usb.rc" "import /init.qcom.power.rc\n";
 
+backup_file init.oem_ftm.rc
+remove_section init.oem_ftm.rc "service ftm_power_config" "oneshot"
+
 # end ramdisk changes
 
 write_boot;
