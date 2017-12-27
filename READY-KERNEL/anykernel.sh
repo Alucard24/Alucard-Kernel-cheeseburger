d16f0605265a2fa5488555c261eaac909ba1ca52
alucard24 <dmbaoh2@gmail.com>
2017-12-27 14:48:02 +0100

    Updated Ramdisk

--- READY-KERNEL/anykernel.sh
+++ READY-KERNEL/anykernel.sh
@@ -2,31 +2,35 @@
 # osm0sis @ xda-developers
 
 ## AnyKernel setup
-# EDIFY properties
+# begin properties
 properties() {
 kernel.string=AlucardKernelEAS by alucard24 @ xda-developers
 do.devicecheck=1
-do.initd=0
 do.modules=1
 do.cleanup=1
-do.cleanuponabort=1
-do.system_blobs=0
+do.cleanuponabort=0
 device.name1=OnePlus5
 device.name2=cheeseburger
-}
+device.name4=
+device.name5=
+} # end properties
 
 # shell variables
 block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
+ramdisk_compression=auto;
 
-## end setup
 
+## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . /tmp/anykernel/tools/ak2-core.sh;
 
-## AnyKernel permissions
-# set permissions for included ramdisk files
-chmod -R 755 $ramdisk
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+chmod -R 750 $ramdisk/*;
+chown -R root:root $ramdisk/*;
+
 
 ## AnyKernel install
 dump_boot;
@@ -35,10 +39,11 @@ dump_boot;
 
 # init.rc
 backup_file init.rc
-insert_line init.rc "init.qcom.power.rc" after "import /init.environ.rc" "import /init.qcom.power.rc\n";
+insert_line init.rc "init.qcom.power.rc" after "import init.qcom.usb.rc" "import /init.qcom.power.rc\n";
 
 # end ramdisk changes
 
 write_boot;
 
 ## end install
+
