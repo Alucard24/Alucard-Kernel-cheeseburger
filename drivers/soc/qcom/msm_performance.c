4967e78b2e88b371a6e211cc5da5a0cfc6bbc52d
Alucard24 <dmbaoh2@gmail.com>
2018-08-06 11:35:44 +0200

    Revert "msm: performance: prevent the driver from handling userspace min freq cpu boost hints. We'll use the standard cpu-boost input-boost functionality which gives users freedom to choose their own settings"
    This reverts commit 0a295cc5a03563609e7f4af23aaf9b0fe93e5c7e.

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -426,7 +426,6 @@ device_param_cb(touchboost, &param_ops_touchboost, NULL, 0644);
  */
 static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 {
-#if 0
 	int i, j, ntokens = 0;
 	unsigned int val, cpu;
 	const char *cp = buf;
@@ -490,7 +489,6 @@ static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 			cpumask_clear_cpu(j, limit_mask);
 	}
 	put_online_cpus();
-#endif
 
 	return 0;
 }
