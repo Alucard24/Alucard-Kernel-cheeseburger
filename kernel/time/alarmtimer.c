d15a9215f53dcd065cced989b23eb484721f858d
Alucard24 <dmbaoh2@gmail.com>
2018-08-10 11:57:14 +0200

    alarmtimer: add error prints when suspend failed
    The alarmtimer suspend failed when nearest alarm wakeup time is
    less than 2 sec or rtc timer can not start.
    
    In suspend/resume stress testing, we found that sometimes alramtimer
    failed to suspend and hence it cancel the suspend ops. Add error prints
    in suspend failure to provide more info when failure occurs to help
    debugging.
    
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    ---

--- kernel/time/alarmtimer.c
+++ kernel/time/alarmtimer.c
@@ -389,6 +389,8 @@ static int alarmtimer_suspend(struct device *dev)
 
 	if (ktime_to_ns(min) < 2 * NSEC_PER_SEC) {
 		__pm_wakeup_event(ws, 2 * MSEC_PER_SEC);
+		dev_err(dev,
+			"Nearest alarm wakeup time < 2sec, avoiding suspend\n");
 		return -EBUSY;
 	}
 
@@ -400,8 +402,10 @@ static int alarmtimer_suspend(struct device *dev)
 
 	/* Set alarm, if in the past reject suspend briefly to handle */
 	ret = rtc_timer_start(rtc, &rtctimer, now, ktime_set(0, 0));
-	if (ret < 0)
+	if (ret < 0) {
 		__pm_wakeup_event(ws, MSEC_PER_SEC);
+		dev_err(dev, "RTC timer start failed, %d\n", ret);
+	}
 	return ret;
 }
 
